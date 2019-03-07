595fd23f4772b748a2fd7bd56876762934ef96bd
Princesunilkumar <47737076+Princesunilkumar@users.noreply.github.com>
2019-03-07 17:26:58 +0530

    Add files via upload

---
+++ product/src/main/java/com/cts/product/bean/Login.java
@@ -0,0 +1,25 @@
+package com.cts.product.bean;
+
+public class Login {
+	
+	private String loginUserName;
+	private String loginPassword;
+	public String getLoginUserName() {
+		return loginUserName;
+	}
+	public void setLoginUserName(String loginUserName) {
+		this.loginUserName = loginUserName;
+	}
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+	
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
